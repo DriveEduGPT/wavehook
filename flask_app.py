829beceb21c137eaa0aa4aba0ffe0633aff6164b
drivesmart.ck <drivesmart.ck@gmail.com>
2025-09-24 22:54:03 +0800

    first version

---
+++ flask_app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # Webhook handling logic will go here
+    print(request.json)
+    return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
