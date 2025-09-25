3c8e5296b78e2c90d246267d620f11a54fa63673
drivesmart.ck <drivesmart.ck@gmail.com>
2025-09-25 16:55:45 +0800

    更新 Flask-SocketIO 設定以允許 CORS，並新增 PythonAnywhere 部署指令以設置網站和環境變數

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback_secret_key_if_not_set') # Change this to a strong secret key
-socketio = SocketIO(app, async_mode='gevent')
+socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins=['https://wavehook.pythonanywhere.com', 'http://127.0.0.1:5000'])
 
 message_history = [] # Initialize with an empty list to store all messages
 
